c6c0982043ae91f52c05f6e5a98246fcdcbc982b
jirmskie9 <70647186+jirmskie9@users.noreply.github.com>
2024-11-26 11:45:17 +0800

    Update header.php

--- pages/header.php
+++ pages/header.php
@@ -15,7 +15,3 @@
   <!-- CSS Files -->
   <link id="pagestyle" href="../assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
   <script src = "sweetalert.min.js"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
-  
